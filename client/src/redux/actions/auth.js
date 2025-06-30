8c52e32e0444bb3dc5ea0a2df088109638ec74d0
pavelihno <crazzyyellow02@gmail.com>
2025-06-30 16:01:06 +0200

    Изменены права доступа для файлов и добавлены действия аутентификации в Redux

---
+++ client/src/redux/actions/auth.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { serverApi, setAuthToken } from '../../api';
+import { getErrorData } from '../utils';
+import { setCurrentUser } from '../reducers/auth';
+
+export const login = createAsyncThunk(
+    'auth/login',
+    async (formData, { rejectWithValue }) => {
+        try {
+            const res = await api.post('/login', formData);
+            const { token, user } = res.data;
+            setAuthToken(token);
+            return user;
+        } catch (err) {
+            return rejectWithValue(getErrorData(err));
+        }
+    }
+);
+
+export const register = createAsyncThunk(
+    'auth/register',
+    async (formData, { rejectWithValue }) => {
+        try {
+            const res = await api.post('/register', formData);
+            const { token, user } = res.data;
+            setAuthToken(token);
+            return user;
+        } catch (err) {
+            return rejectWithValue(getErrorData(err));
+        }
+    }
+);
+
+export const logout = createAsyncThunk('auth/logout', () => {
+    setAuthToken(false);
+    setCurrentUser(null);
+});
+
+export const auth = createAsyncThunk(
+    'auth',
+    async () => {
+        const res = await api.get('/auth');
+        const { user } = res.data;
+        return user;
+    }
+);
