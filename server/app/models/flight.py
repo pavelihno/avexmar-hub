39589970293889347484d71150f88f5f67fcdff6
Pavel <73386000+pavelihno@users.noreply.github.com>
2025-06-30 18:54:22 +0200

    Extend BaseModel fields and clean up models

--- server/app/models/flight.py
+++ server/app/models/flight.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from database import db
 from models.base_model import BaseModel
 
@@ -7,7 +5,6 @@ from models.base_model import BaseModel
 class Flight(BaseModel):
     __tablename__ = 'flights'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     number = db.Column('flight_number', db.String(), nullable=False)
     origin_airport = db.Column('origin', db.String(), nullable=False)
     destination_airport = db.Column('destination', db.String(), nullable=False)
