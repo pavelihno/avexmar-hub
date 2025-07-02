282f550353d683db049417a03d5c4b44e853e206
Pavel <73386000+pavelihno@users.noreply.github.com>
2025-07-02 12:19:03 +0200

    Remove migration and revert controller

--- server/app/models/seat.py
+++ server/app/models/seat.py
@@ -6,18 +6,20 @@ class Seat(BaseModel):
     __tablename__ = 'seats'
 
     seat_number = db.Column(db.String(10), nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey('flights.id', ondelete='RESTRICT'), nullable=False)
     tariff_id = db.Column(db.Integer, db.ForeignKey('tariffs.id', ondelete='RESTRICT'), nullable=False)
     passenger_id = db.Column(db.Integer, db.ForeignKey('passengers.id', ondelete='RESTRICT'), nullable=True)
     booking_id = db.Column(db.Integer, db.ForeignKey('bookings.id', ondelete='SET NULL'), nullable=True)
 
     __table_args__ = (
-        db.UniqueConstraint('tariff_id', 'seat_number'),
+        db.UniqueConstraint('flight_id', 'seat_number'),
     )
 
     def to_dict(self):
         return {
             'id': self.id,
             'seat_number': self.seat_number,
+            'flight_id': self.flight_id,
             'tariff_id': self.tariff_id,
             'passenger_id': self.passenger_id,
             'booking_id': self.booking_id
