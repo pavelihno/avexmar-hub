eedae0480f07388c10449f5727c4af082b22ec31
pavelihno <crazzyyellow02@gmail.com>
2025-07-17 07:57:50 +0200

    removed focus warning

--- client/src/components/auth/AuthModal.js
+++ client/src/components/auth/AuthModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Modal, Fade } from '@mui/material';
 
 import Login from './Login';
@@ -11,19 +11,35 @@ const AuthModal = () => {
 	const { authModal, closeAuthModal } = useAuthModal();
 	const { isOpen, type } = authModal;
 
+	const lastFocusedRef = useRef(null);
+
+	useEffect(() => {
+		if (isOpen) {
+			lastFocusedRef.current = document.activeElement;
+		}
+	}, [isOpen]);
+
+	const handleExited = () => {
+		const node = lastFocusedRef.current;
+		if (node && typeof node.focus === 'function') {
+			node.focus();
+		}
+	};
+
 	return (
 		<Modal
 			open={isOpen}
 			onClose={closeAuthModal}
 			aria-labelledby='auth-modal'
 			closeAfterTransition
+			disableRestoreFocus
 			sx={{
 				display: 'flex',
 				alignItems: 'center',
 				justifyContent: 'center',
 			}}
 		>
-			<Fade in={isOpen}>
+			<Fade in={isOpen} onExited={handleExited}>
 				<div>
 					{type === 'login' && <Login isModal={true} />}
 					{type === 'register' && <Register isModal={true} />}
