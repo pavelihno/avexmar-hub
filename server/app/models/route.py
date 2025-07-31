808e53a50c42fce1c7dc139eeee9f62571c44ed0
pavelihno <crazzyyellow02@gmail.com>
2025-07-31 22:17:02 +0200

    Add unique constraint for route origin and destination airports

--- server/app/models/route.py
+++ server/app/models/route.py
@@ -8,6 +8,13 @@ class Route(BaseModel):
     origin_airport_id = db.Column(db.Integer, db.ForeignKey('airports.id', ondelete='RESTRICT'), nullable=False)
     destination_airport_id = db.Column(db.Integer, db.ForeignKey('airports.id', ondelete='RESTRICT'), nullable=False)
 
+    __table_args__ = (
+        db.UniqueConstraint(
+            'origin_airport_id', 'destination_airport_id',
+            name='uix_origin_destination_airport'
+        ),
+    )
+
     def to_dict(self):
         return {
             'id': self.id,
