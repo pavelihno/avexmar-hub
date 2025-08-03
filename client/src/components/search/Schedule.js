eb700da5b0dc2b6989e2a0826e4e5936206edb2f
pavelihno <crazzyyellow02@gmail.com>
2025-08-03 08:37:41 +0300

    Refactor Schedule and ScheduleTable components for improved readability and consistency; update UI_LABELS for better localization support.

--- client/src/components/search/Schedule.js
+++ client/src/components/search/Schedule.js
@@ -11,14 +11,14 @@ import { fetchAirlines } from '../../redux/actions/airline';
 import { formatDate } from '../utils';
 
 const Schedule = () => {
-        const dispatch = useDispatch();
-        const { flights } = useSelector((state) => state.search);
-        const { airlines } = useSelector((state) => state.airlines);
-        const [params] = useSearchParams();
-        const paramObj = Object.fromEntries(params.entries());
-        const paramStr = params.toString();
-        const from = params.get('from');
-        const to = params.get('to');
+	const dispatch = useDispatch();
+	const { flights } = useSelector((state) => state.search);
+	const { airlines } = useSelector((state) => state.airlines);
+	const [params] = useSearchParams();
+	const paramObj = Object.fromEntries(params.entries());
+	const paramStr = params.toString();
+	const from = params.get('from');
+	const to = params.get('to');
 
 	useEffect(() => {
 		if (from && to) {
@@ -29,58 +29,49 @@ const Schedule = () => {
 		}
 	}, [dispatch, paramStr, from, to]);
 
-        useEffect(() => {
-                document.title = UI_LABELS.SCHEDULE.from_to(from || '', to || '');
-                return () => {
-                        document.title = UI_LABELS.APP_TITLE;
-                };
-        }, [from, to]);
+	useEffect(() => {
+		document.title = UI_LABELS.SCHEDULE.from_to(from || '', to || '');
+		return () => {
+			document.title = UI_LABELS.APP_TITLE;
+		};
+	}, [from, to]);
 
-        useEffect(() => {
-                dispatch(fetchAirlines());
-        }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchAirlines());
+	}, [dispatch]);
 
-        const outboundFlights = useMemo(
-                () => flights.filter((f) => f.direction === 'outbound'),
-                [flights],
-        );
+	const outboundFlights = useMemo(() => flights.filter((f) => f.direction === 'outbound'), [flights]);
 
-        const returnFlights = useMemo(
-                () => flights.filter((f) => f.direction === 'return'),
-                [flights],
-        );
+	const returnFlights = useMemo(() => flights.filter((f) => f.direction === 'return'), [flights]);
 
 	return (
 		<Base maxWidth='xl'>
 			<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 				<SearchForm initialParams={paramObj} />
 			</Box>
-                        <Box sx={{ p: 3 }}>
-                                <Typography variant='subtitle1'>
-                                        {UI_LABELS.SCHEDULE.from_to(from || '', to || '')}
-                                </Typography>
-                                <Typography variant='h4' component='h1' gutterBottom sx={{ mt: 3 }}>
-                                        {UI_LABELS.SCHEDULE.title}
-                                </Typography>
-                                <Typography variant='h5' sx={{ mt: 3, mb: 1 }}>
-                                        {UI_LABELS.SCHEDULE.outbound}
-                                </Typography>
-                                {outboundFlights.length ? (
-                                        <ScheduleTable flights={outboundFlights} airlines={airlines} />
-                                ) : (
-                                        <Typography>{UI_LABELS.SCHEDULE.no_results}</Typography>
-                                )}
-                                <Typography variant='h5' sx={{ mt: 4, mb: 1 }}>
-                                        {UI_LABELS.SCHEDULE.return}
-                                </Typography>
-                                {returnFlights.length ? (
-                                        <ScheduleTable flights={returnFlights} airlines={airlines} />
-                                ) : (
-                                        <Typography>{UI_LABELS.SCHEDULE.no_results}</Typography>
-                                )}
-                        </Box>
-                </Base>
-        );
+			<Box sx={{ p: 3 }}>
+				<Typography variant='h4' component='h1' gutterBottom sx={{ mt: 3 }}>
+					{UI_LABELS.SCHEDULE.title}
+				</Typography>
+				<Typography variant='subtitle1' sx={{ fontWeight: 'bold', mt: 3, mb: 1 }}>
+					{UI_LABELS.SCHEDULE.from_to(from || '', to || '')}
+				</Typography>
+				{outboundFlights.length ? (
+					<ScheduleTable flights={outboundFlights} airlines={airlines} />
+				) : (
+					<Typography>{UI_LABELS.SCHEDULE.no_results}</Typography>
+				)}
+				<Typography variant='subtitle1' sx={{ fontWeight: 'bold', mt: 4, mb: 1 }}>
+					{UI_LABELS.SCHEDULE.from_to(to || '', from || '')}
+				</Typography>
+				{returnFlights.length ? (
+					<ScheduleTable flights={returnFlights} airlines={airlines} />
+				) : (
+					<Typography>{UI_LABELS.SCHEDULE.no_results}</Typography>
+				)}
+			</Box>
+		</Base>
+	);
 };
 
 export default Schedule;
