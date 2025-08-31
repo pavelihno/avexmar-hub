1e3be948940f53658b81d9c557542f1f382c3756
Pavel <73386000+pavelihno@users.noreply.github.com>
2025-08-31 16:01:19 +0300

    feat: add home page poster carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { Box } from '@mui/material';
 import Base from './Base';
 import SearchForm from './search/SearchForm';
+import PosterCarousel from './home/PosterCarousel';
 
 const Home = () => {
 	return (
 		<Base maxWidth='xl'>
-			<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-				<Box sx={{ width: '100%', px: { xs: 1, sm: 2 } }}>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: { xs: 'column', md: 'row' },
+					justifyContent: 'center',
+					alignItems: 'center',
+					gap: 2,
+				}}
+			>
+				<Box sx={{ width: '100%', px: { xs: 1, sm: 2 }, flex: 1 }}>
 					<SearchForm loadLocalStorage />
+				</Box>
+				<Box sx={{ width: '100%', flex: 1, mt: { xs: 2, md: 0 } }}>
+					<PosterCarousel />
 				</Box>
 			</Box>
 		</Base>
---
+++ client/src/constants/posters.js
@@ -0,0 +1,19 @@
+const POSTERS = [
+	{
+		src: '/images/posters/winter.svg',
+		title: 'Winter Deals',
+		href: '/deals',
+	},
+	{
+		src: '/images/posters/routes.svg',
+		title: 'New Routes',
+		href: '/routes',
+	},
+	{
+		src: '/images/posters/loyalty.svg',
+		title: 'Loyalty Program',
+		href: '/loyalty',
+	},
+];
+
+export default POSTERS;
