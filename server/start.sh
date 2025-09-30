522d46e821e11e50e2cbf183b0435134dea6786f
pavelihno <crazzyyellow02@gmail.com>
2025-09-30 12:27:19 +0200

    added monitoring using grafana

--- server/start.sh
+++ server/start.sh
@@ -6,9 +6,19 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 APP_DIR="$SCRIPT_DIR/app"
 SCHEDULE_DIR="$APP_DIR/__pycache__"
-LOG_DIR="$SCRIPT_DIR/../logs"
+LOG_DIR="$SCRIPT_DIR/logs"
+LOG_SUBDIRS=(
+  "supervisord"
+  "gunicorn"
+  "celery"
+  "caddy"
+)
 
 mkdir -p "$SCHEDULE_DIR"
 mkdir -p "$LOG_DIR"
+
+for dir in "${LOG_SUBDIRS[@]}"; do
+  mkdir -p "$LOG_DIR/$dir"
+done
 
 exec supervisord -c "$SCRIPT_DIR/supervisord.conf"
